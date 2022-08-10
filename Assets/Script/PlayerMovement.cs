f5731501fac49ee46aac5bc771452a01bfba19a9
Tobias <tk@multiconsult.no>
2022-08-10 08:25:21 +0200

    Updated to physics based movement
     using rigidbody

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+<<<<<<< Updated upstream
+=======
+    // Variable player input
+    [SerializeField] public float moveSpeed = 0f;
+    [SerializeField] private float maxSpeed = 1.5f;
+    [SerializeField] private float acceleration = 1000f;
+    [SerializeField] private float deacceleration = 0.9f;
+    [SerializeField] private float rotationSpeed = 600f;
+    [SerializeField] public float timeBetweenAttack = 0.2f;
+>>>>>>> Stashed changes
 
     [SerializeField] public float moveSpeed = 1.5f;
     [SerializeField] private float rotationSpeed;
@@ -45,18 +55,57 @@ public class PlayerMovement : MonoBehaviour
 
     void ProcessInputs()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
 
+<<<<<<< Updated upstream
         moveDirection = new Vector2(moveX, moveY);
+=======
+
+        moveDirection = new Vector2(moveX,moveY);
+>>>>>>> Stashed changes
         inputMagnitude = Mathf.Clamp01(moveDirection.magnitude);
         moveDirection.Normalize();
     }
 
     private void Move()
     {
+<<<<<<< Updated upstream
        
         transform.Translate(moveDirection * moveSpeed * inputMagnitude * Time.deltaTime, Space.World);
+=======
+        //Physics based movement https://answers.unity.com/questions/616195/how-to-make-an-object-go-the-direction-it-is-facin.html
+        if (Input.GetButton("w"))
+        {
+            moveSpeed += acceleration;
+            Debug.Log("Acceleration+\\" + moveSpeed + "moveDirection: " + moveDirection + "inputMagnitude: " + inputMagnitude);
+            //transform.Translate(moveDirection * moveSpeed * inputMagnitude * Time.deltaTime, Space.World);
+            rb.AddForce(moveDirection * acceleration);
+
+        }
+
+        //else if ((Input.GetButton("Vertical") || Input.GetButton("Horizontal")) && moveSpeed == maxSpeed)
+        //{
+        //    Debug.Log("Acceleration+\\" + moveSpeed + "moveDirection: " + moveDirection + "inputMagnitude: " + inputMagnitude);
+        //    transform.Translate(moveDirection * moveSpeed * inputMagnitude * Time.deltaTime, Space.World);
+        //}
+        ////Deacceleration 
+        //else if (moveSpeed > 0)
+        //{
+        //    moveSpeed += -deacceleration;
+        //    //inputMagnitude = 1;
+        //    Debug.Log("Deacceleration-//" + moveSpeed + "moveDirection: " + moveDirection + "inputMagnitude: " + inputMagnitude);
+        //}
+
+        //else
+        //{
+        //    transform.Translate(moveDirection * moveSpeed * inputMagnitude * Time.deltaTime, Space.World);
+        //}
+
+
+        //Move command
+        //transform.Translate(moveDirection * moveSpeed * inputMagnitude * Time.deltaTime, Space.World);
+>>>>>>> Stashed changes
 
         if (moveDirection != Vector2.zero)
         {
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
  basic pixel 2d game dev
 
 ![image](https://user-images.githubusercontent.com/79943730/183281334-fa23020f-464b-426a-a6ac-780384cadcb8.png)
+
+##Implementatons:
+Enemy AI - https://forum.unity.com/threads/simple-spaceship-movement-ai-2d.387648/
+Ship movement - https://www.marineinsight.com/naval-architecture/ship-motions/
